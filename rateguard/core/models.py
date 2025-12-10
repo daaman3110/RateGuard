3986b2d7f6c6c3c02bd6355567748d0afef5f30b
daaman3110 <daaman3110@gmail.com>
2025-12-10 20:43:17 +0530

    Added DB Model and Serializers and changed View

--- rateguard/core/models.py
+++ rateguard/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class RequestLog(models.Model):
+    ip_address = models.GenericIPAddressField()
+    path = models.CharField(max_length=255)
+    method = models.CharField(max_length=10)
+    status = models.CharField(max_length=10)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.path}"
--- rateguard/core/views.py
+++ rateguard/core/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import RequestLogSerializer
+from .models import RequestLog
 
 
 # Create your views here.
 @api_view(["GET"])
 def ping(request):
-    return Response({"status": "ok"})
+    logs = RequestLog.objects.all().order_by('-timestamp')
+    serializer = RequestLogSerializer(logs, many=True)
+    return Response(serializer.data)
