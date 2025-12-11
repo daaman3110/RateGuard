f82b58a1786364ae752210e0c9c912e0d5516728
daaman3110 <daaman3110@gmail.com>
2025-12-11 23:54:41 +0530

    Added logging into Postgres

--- rateguard/core/rate_limit_middleware.py
+++ rateguard/core/rate_limit_middleware.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from .redis_service import increment_counter, get_counter
+from .models import RequestLog
 
 RATE_LIMIT = 5  # Allowed Requests per minute
 
@@ -15,11 +16,24 @@ class RateLimitMiddleware:
         # Increment counter for the IP
         count = increment_counter(ip)
 
-        # Check if limit exceeded
+        # Check if limit exceeded -> Block + Log
         if count > RATE_LIMIT:
+            RequestLog.objects.create(
+                ip_address=ip, path=request.path, method=request.method, status=429
+            )
             return JsonResponse(
                 {"error": "Too many requests", "ip": ip, "count": count}, status=429
             )
 
-        # Continue to view
-        return self.get_response(request)
+        # If allowed -> get response from view
+        response = self.get_response(request)
+
+        # Log this request into Postgres
+        RequestLog.objects.create(
+            ip_address=ip,
+            path=request.path,
+            method=request.method,
+            status=str(response.status_code),
+        )
+
+        return response
