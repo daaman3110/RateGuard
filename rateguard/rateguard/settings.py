4bbcdb708d204426c4764cdbe5f726655a70fe87
daaman3110 <daaman3110@gmail.com>
2025-12-11 20:58:37 +0530

    Updated settings and added Middleware

--- rateguard/rateguard/settings.py
+++ rateguard/rateguard/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "core.rate_limit_middleware.RateLimitMiddleware"
 ]
 
 ROOT_URLCONF = "rateguard.urls"
